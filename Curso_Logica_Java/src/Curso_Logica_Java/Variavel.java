45bcef1d605395899421c06f479897b09b432c14
Luck1010 <40967265+Luck1010@users.noreply.github.com>
2021-09-20 01:49:54 -0300

    Add files via upload

---
+++ Curso_Logica_Java/src/Curso_Logica_Java/Variavel.java
@@ -0,0 +1,16 @@
+package Curso_Logica_Java; 
+
+public class Variavel {
+
+	public static void main(String[] args) {
+		
+		int idade; // a variável idade armazena a idade da pessoa 
+		idade = 25; //este conceito atribui um valor a idade da pessoa
+		
+		System.out.print("A minha idade é: " + idade ); // esse comando exibe o resultado na tela
+		
+		
+		 
+	}
+
+}
